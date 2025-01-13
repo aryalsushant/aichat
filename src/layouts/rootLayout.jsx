6df359aedcd47a76c5d6b26065d51020ef81fd1f
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-12 23:12:57 -0600

    layouts added

---
+++ src/layouts/rootLayout.jsx
@@ -0,0 +1,20 @@
+import './rootLayout.css'
+
+const RootLayout = () => {
+    return (
+        <div className='rootLayout'>
+            <header>
+                <Link to = "/">
+                <img src = "/logo.png" alt = "" />
+                <span>Brainrot AI</span>
+                </Link>
+            </header>
+
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    )
+}
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/homepage';
+import DashboardPage from './routes/dashboardpage/dashboardpage';
+import ChatPage from './routes/chatpage/chatpage';
+
+const router = createBrowserRouter([
+ {
+  element: <RootLayout/>,
+  childern: [
+    {
+      path: "/", element: <Homepage/>
+    }
+  ]
+ },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
