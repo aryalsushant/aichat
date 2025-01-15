40ebd4aa1059b14ed7b1d167447742c58431f705
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-15 15:51:51 -0600

    homepage left flexbox design completed

--- src/routes/homepage/homepage.jsx
+++ src/routes/homepage/homepage.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
     return (
         <div className='homepage'>
+            <img src = "/orbital.png" className = "orbital" />
             <div className="left">
-                <h1>Brainrot AI</h1>
+                <h1>BrainrotAI</h1>
                 <h2>Talk to AI huzz cause you can't get nun irl</h2>
                 <h3>Pull up and get fluent in Gen Z slang wit ya AI bestie! I’ll keep you in da loop, put you on trends, and level up ya text game. Whether you tryna vibe or just not be mad lost, I gotchu. No cap, it’s light work.</h3>
-                <button>Get Started</button>
+                <Link to = "/dashboard">Get Started</Link>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="imgContainer">
+                    
+                </div>
+            </div>
         </div>
     )
 }
