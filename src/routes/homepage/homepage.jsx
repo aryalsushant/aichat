191c5930cf6b9a512c602c2c2553541e5b7f1fe7
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-15 14:49:02 -0600

    home page left elements created

--- src/routes/homepage/homepage.jsx
+++ src/routes/homepage/homepage.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
     return (
         <div className='homepage'>
-            <Link to = "/dashboard">Dashboard</Link>
+            <div className="left">
+                <h1>Brainrot AI</h1>
+                <h2>Talk to AI huzz cause you can't get nun irl</h2>
+                <h3>Pull up and get fluent in Gen Z slang wit ya AI bestie! I’ll keep you in da loop, put you on trends, and level up ya text game. Whether you tryna vibe or just not be mad lost, I gotchu. No cap, it’s light work.</h3>
+                <button>Get Started</button>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
