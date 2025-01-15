d072002b4c5d4d3a7ee18277851607d50e9864d3
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-14 22:41:19 -0600

    user info div added

--- src/layouts/rootLayout/rootLayout.jsx
+++ src/layouts/rootLayout/rootLayout.jsx
@@ -1,8 +1,19 @@
 import {Link, Outlet} from "react-router-dom";
 import "./rootLayout.css";
+import { ClerkProvider } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 const RootLayout = () => {
     return (
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      
+    
         <div className='rootLayout'>
             <header>
                 <Link to = "/" className="logo">
@@ -10,13 +21,21 @@ const RootLayout = () => {
                 <span>Brainrot AI</span>
                 </Link>
 
-                <div className="user">User</div>
+                <div className="user">
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                </div>
             </header>
 
             <main>
                 <Outlet/>
             </main>
         </div>
+        </ClerkProvider>
     );
 };
 
