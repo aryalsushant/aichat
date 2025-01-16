ebeb07035bba907f82b7270130e7fd088e18ffa2
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-16 12:23:46 -0600

    dashboard options created

--- src/routes/dashboardpage/dashboardpage.jsx
+++ src/routes/dashboardpage/dashboardpage.jsx
@@ -2,7 +2,32 @@ import './dashboardpage.css'
 
 const DashboardPage = () => {
     return (
-        <div className='dashboardpage'>Dashboard</div>
+        <div className='dashboardpage'>
+            <div className="texts">
+                <div className="logo">
+                    <img src="/ogo.png" alt="" />
+                    <h1>Brainrot AI</h1>
+                </div>
+                <div className="options">
+                    <div className="option">
+                        <img src="/chat.png" alt="" />
+                        <span>Create a New Chat</span>
+                    </div>
+
+                    <div className="option">
+                        <img src="/chat.png" alt="" />
+                        <span>Create a New Chat</span>
+                    </div>
+
+                    <div className="option">
+                        <img src="/chat.png" alt="" />
+                        <span>Create a New Chat</span>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="formContainer"></div>
+        </div>
     )
 }
 
