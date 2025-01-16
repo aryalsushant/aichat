44fd689edb11f6afba1f7e85f81d10ea2ad8071c
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-15 22:55:31 -0600

    type animation added

--- src/routes/homepage/homepage.jsx
+++ src/routes/homepage/homepage.jsx
@@ -1,5 +1,7 @@
 import './homepage.css'
 import { Link } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const Homepage = () => {
     return (
@@ -19,6 +21,27 @@ const Homepage = () => {
                         </div>
                     </div>
                     <img src = "/bot.png" className = "bot"/>
+
+                    <div className="chat">
+                    
+                    <TypeAnimation
+                        sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'We produce food for Mice',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'We produce food for Hamsters',
+                        1000,
+                        'We produce food for Guinea Pigs',
+                        1000,
+                        'We produce food for Chinchillas',
+                        1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{ fontSize: '2em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                    </div>
                 </div>
             </div>
         </div>
