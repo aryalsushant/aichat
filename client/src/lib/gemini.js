360c81ff985cef25c72f4a8833d6b9652772a7f4
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-20 23:13:36 -0500

    gemini ai

---
+++ client/src/lib/gemini.js
@@ -0,0 +1,4 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_PUBLIC_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
