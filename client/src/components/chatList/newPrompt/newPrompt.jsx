e26898df972b572decfcd1ee956b102d76142741
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-20 22:39:30 -0500

    upload image button still not visible

--- client/src/components/chatList/newPrompt/newPrompt.jsx
+++ client/src/components/chatList/newPrompt/newPrompt.jsx
@@ -1,18 +1,42 @@
 import './newPrompt.css'
 import { useRef, useEffect } from 'react';
+import Upload from "../upload/Upload";
 
 const NewPrompt = () => {
+
+    const[img,setImg] = useState({
+        isLoading: false,
+        error: "",
+        dbData: {}
+
+    })
 
     const endRef = useRef(null);
 
     useEffect(() => {
         endRef.current.scrollIntoView({behavior: "smooth"})
-    })
+    }, [])
     return (
         <>
+        {/*ADD NEW CHAT*/}
+        {img.isLoading && <div className="">Loading...</div>}
+        {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+        )}
+        {question && <div className="message user">{question}</div>}
+        {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+         )}
         <div className="endChat" ref = {endRef}></div>
-            <form className = "newForm" action="">
-                <Upload /> //this is not visible and needs to be fixed
+            <form className = "newForm" >
+                <Upload setImg = {setImg} /> 
                 <input id = "file" type="file" multiple = {false} hidden />
                 <input type="text" placeholder='Ask anything...' />
                 <button>
