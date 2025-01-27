82fe42955b789f1dcddd33fc04fab12971af3469
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-27 10:38:37 -0600

    chatlist loading issue fixed (missing value on env)

--- client/src/components/chatList/chatList.jsx
+++ client/src/components/chatList/chatList.jsx
@@ -1,39 +1,45 @@
-import './chatList.css'
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./chatList.css";
+import { useQuery } from "@tanstack/react-query";
 
 const ChatList = () => {
-   return (
+  const { isPending, error, data } = useQuery({
+    queryKey: ["userChats"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  return (
     <div className="chatList">
-    <span className = "title">DASHBOARD</span>
-    <Link to = "/dashboard">Create a new chat</Link>
-    <Link>Explore Brainrot AI</Link>
-    <Link>Contact</Link>
-
-    <hr/>
-    <span className = "title">RECENT CHATS</span>
-    <div className="list">
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-        <Link>My chat title</Link>
-    </div>
-    <hr/>
-    <div className="upgrade">
-        <img src = "/logo.png"/>
+      <span className="title">DASHBOARD</span>
+      <Link to="/dashboard">Create a new Chat</Link>
+      <Link to="/">Explore Lama AI</Link>
+      <Link to="/">Contact</Link>
+      <hr />
+      <span className="title">RECENT CHATS</span>
+      <div className="list">
+        {isPending
+          ? "Loading..."
+          : error
+          ? "Something went wrong!"
+          : data?.map((chat) => (
+              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                {chat.title}
+              </Link>
+            ))}
+      </div>
+      <hr />
+      <div className="upgrade">
+        <img src="/logo.png" alt="" />
         <div className="texts">
-            <span>Upgrade to Brainrot Premium</span>
-            <span>Get unlimited access to all features</span>
+          <span>Upgrade to Brot AI Pro</span>
+          <span>Get unlimited access to all features</span>
         </div>
+      </div>
     </div>
-
-    </div>
-   )
-}
+  );
+};
 
 export default ChatList;
