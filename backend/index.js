840a89300d820e83fee8dcf334e3f595923d7dfe
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-27 10:26:08 -0600

    fetching chat data from mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,98 +1,153 @@
 import express from "express";
-import ImageKit from "imagekit";
 import cors from "cors";
+import path from "path";
+import url, { fileURLToPath } from "url";
+import ImageKit from "imagekit";
 import mongoose from "mongoose";
 import Chat from "./models/chat.js";
-import UserChats from "./models/userChats.js" 
+import UserChats from "./models/userChats.js";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-}));
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-//either this
 const connect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to MongoDB");
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const imagekit = new ImageKit({
-    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
-    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
 });
 
+app.get("/api/upload", (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
 
+app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+  const { text } = req.body;
 
-app.get("/api/upload",(req, res) => {
-    const result = imagekit.getAuthenticationParameters();
-    res.send("it works!");
-})
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    });
 
-app.get("/api/test", ClerkExpressRequireAuth(), (req, res) =>{
-  console.log("Success")
-  res.send("Success!")
-})
+    const savedChat = await newChat.save();
 
-app.post("/api/chats",
-  ClerkExpressRequireAuth(),
-  async (req, res) => {
-    const {userId, text} = req.body
+    // CHECK IF THE USERCHATS EXISTS
+    const userChats = await UserChats.find({ userId: userId });
 
-
-    try{
-
-      //Creating a new chat
-
-      const newChat = new Chat({
-        userId: userId, 
-        history:[{role:"user", parts:[{text}]}]
+    // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
       });
 
-      const savedChat = await newChat.save();
-
-      //Checking if the userchat exists
-      const userChats = await UserChats.find({userId:userId});
-      //if it doesn't, we create a new one
-      if(!userChats.length){
-        const newUserChats = new UserChats({
-          userId:userId,
-          chats: [
-            {
-              _id:savedChat.id,
-              title: text.substring(0,40),
-
-              
-            }
-          ]
-        });
-        await newUserChats.save();
-      }else {
-        //if it exists, we push it to existing array
-        await UserChats.updateOne({userId:userId}, {
+      await newUserChats.save();
+    } else {
+      // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+      await UserChats.updateOne(
+        { userId: userId },
+        {
           $push: {
             chats: {
-              _id:savedChat._id,
-              title: text.substring(0,40),
-            }
-          }
-        })
-      }
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
 
-    }catch(err){
-      console.log(err)
-      res.status(500).send("Error creating chat!")
+      res.status(201).send(newChat._id);
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error creating chat!");
+  }
+});
+
+app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const userChats = await UserChats.find({ userId });
+
+    res.status(200).send(userChats[0].chats);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching userchats!");
+  }
+});
+
+app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const chat = await Chat.findOne({ _id: req.params.id, userId });
+
+    res.status(200).send(chat);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error fetching chat!");
+  }
+});
+
+app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  const { question, answer, img } = req.body;
+
+  const newItems = [
+    ...(question
+      ? [{ role: "user", parts: [{ text: question }], ...(img && { img }) }]
+      : []),
+    { role: "model", parts: [{ text: answer }] },
+  ];
+
+  try {
+    const updatedChat = await Chat.updateOne(
+      { _id: req.params.id, userId },
+      {
+        $push: {
+          history: {
+            $each: newItems,
+          },
+        },
+      }
+    );
+    res.status(200).send(updatedChat);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error adding conversation!");
+  }
 });
 
 app.use((err, req, res, next) => {
@@ -100,9 +155,14 @@ app.use((err, req, res, next) => {
   res.status(401).send("Unauthenticated!");
 });
 
-app.listen(port, () => {
-    connect();
-    console.log("Server running on 3000");
-  });
+// PRODUCTION
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  //or this chunk needs work
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+
+app.listen(port, () => {
+  connect();
+  console.log("Server running on 3000");
+});
