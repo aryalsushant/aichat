b1b7d005f3bea6f0dabd5a51a7cac363dac899c7
Sushant Aryal <sushantaryal05@gmail.com>
2025-01-27 23:16:29 -0600

    chat loading wit mongodb issue fixed

--- client/src/routes/chatpage/chatpage.jsx
+++ client/src/routes/chatpage/chatpage.jsx
@@ -6,56 +6,51 @@ import Markdown from "react-markdown";
 import { IKImage } from "imagekitio-react";
 
 const ChatPage = () => {
-  const path = useLocation().pathname;
-  const chatId = path.split("/").pop();
+    const path = useLocation().pathname;
+    const chatId = path.split("/").pop();
 
-  const { isPending, error, data } = useQuery({
-    queryKey: ["chat", chatId],
-    queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
-        credentials: "include",
-      }).then((res) => res.json()),
-  });
+    const { isPending, error, data } = useQuery({
+        queryKey: ["chat", chatId],
+        queryFn: () =>
+            fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+                credentials: "include",
+            }).then((res) => res.json()),
+    });
 
-  console.log(data);
+    return (
+        <div className="chatPage">
+            <div className="wrapper">
+                <div className="chat">
+                    {isPending ? (
+                        "Loading..."
+                    ) : error ? (
+                        "Something went wrong!"
+                    ) : (
+                        data?.history?.map((message, i) => (
+                            <div key={`message-${i}`}>
+                                {message.img && (
+                                    <IKImage
+                                        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                                        path={message.img}
+                                        height="300"
+                                        width="400"
+                                        transformation={[{ height: 300, width: 400 }]}
+                                        loading="lazy"
+                                        lqip={{ active: true, quality: 20 }}
+                                    />
+                                )}
+                                <div className={message.role === "user" ? "message user" : "message"}>
+                                    <Markdown>{message.parts[0].text}</Markdown>
+                                </div>
+                            </div>
+                        ))
+                    )}
 
-  return (
-    <div className="chatPage">
-      <div className="wrapper">
-        <div className="chat">
-          {isPending
-            ? "Loading..."
-            : error
-            ? "Something went wrong!"
-            : data?.history?.map((message, i) => (
-                <>
-                  {message.img && (
-                    <IKImage
-                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-                      path={message.img}
-                      height="300"
-                      width="400"
-                      transformation={[{ height: 300, width: 400 }]}
-                      loading="lazy"
-                      lqip={{ active: true, quality: 20 }}
-                    />
-                  )}
-                  <div
-                    className={
-                      message.role === "user" ? "message user" : "message"
-                    }
-                    key={i}
-                  >
-                    <Markdown>{message.parts[0].text}</Markdown>
-                  </div>
-                </>
-              ))}
-
-          {data && <NewPrompt data={data}/>}
+                    {data && <NewPrompt data={data} />}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ChatPage;
